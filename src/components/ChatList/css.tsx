6419b19a3a1c92cc1d65645b36436c623643bc62
jerry <1052557325@qq.com>
2023-04-05 21:50:21 +0800

    feat: alter margin

--- src/components/ChatList/css.tsx
+++ src/components/ChatList/css.tsx
@@ -6,7 +6,7 @@ export const ChatListContainer = styled.div`
    display: flex;
   flex-direction: column;
   gap: 5px;
-  margin-bottom: 180px;
+  margin-bottom: 120px;
   margin-top: 60px;
 `
 const nightContentTheme = css`
