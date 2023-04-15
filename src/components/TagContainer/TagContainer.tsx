0eed446502eeb6698511da6b2845bdcd5aa270de
jerry <1052557325@qq.com>
2023-04-15 14:31:56 +0800

    replace bottom left tag with new tag

--- src/components/TagContainer/TagContainer.tsx
+++ src/components/TagContainer/TagContainer.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import {Tag} from "antd";
 import {apiModelName} from "../../util/constanst";
-import {PositionTag} from "./css";
+import {PositionTag, Tag, TagText} from "./css";
 import useAllStates from "../../hooks/useAllStates";
 
-function TagContainer(){
+function TagContainer() {
     const {theme} = useAllStates()
-    return(
+    return (
         <PositionTag theme={theme}>
-            <Tag color={'#2db7f5'}>Copyright@Jerry</Tag>
-            <Tag color={'#2db7f5'}>{apiModelName}</Tag>
+            <Tag start={'#fbb034'} end={'#ffdd00'}><TagText color={'#645928'}>Copyright@Jerry</TagText></Tag>
+            <Tag start={'#63a4ff'} end={'#83eaf1'}><TagText color={'#1e4656'}>{apiModelName}</TagText></Tag>
         </PositionTag>
     )
 }
--- src/components/TagContainer/css.tsx
+++ src/components/TagContainer/css.tsx
@@ -13,3 +13,17 @@ export const PositionTag = styled.div<{ theme: Theme }>`
     align-items: center;
     background-color: ${p => p.theme === Theme.day ? '#ffffff' : '#041527'};
 `
+export const Tag = styled.div<{start: string, end: string}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 28px;
+  border-radius: 20px;
+  background-color: ${p => p.start};
+  background-image: linear-gradient(315deg, ${p=> p.start} 0%, ${p=> p.end} 74%);
+`
+export const TagText = styled.div<{color: string}>`
+  font-size: 13px;
+  color: ${p=> p.color};
+  padding: 0 10px;
+`
